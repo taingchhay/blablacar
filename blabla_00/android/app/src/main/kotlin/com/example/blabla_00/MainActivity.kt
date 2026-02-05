5627280a8f09b189100397b1cc4ab6cc3adbcddf
taingchhay <filmakame@gmail.com>
2026-02-05 12:00:23 +0700

    blabla-00-startcode

---
+++ blabla_00/android/app/src/main/kotlin/com/example/blabla_00/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blabla_00
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ blabla_00/lib/services/rides_service.dart
@@ -0,0 +1,13 @@
+import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+
+ 
+}
